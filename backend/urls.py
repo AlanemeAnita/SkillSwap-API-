8e6cfd451c378fb4352ec76789281e060d1949af
Alaneme Anita <anitaalaneme@gmail.com>
2025-10-01 20:33:01 +0100

    add authentication and relationships

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.views import PostViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -9,4 +13,7 @@ router.register(r'posts', PostViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),   # our API lives here
+    path("api/", include("core.urls")),  # your posts, etc.
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello from Core app!")
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
