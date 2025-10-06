7347f28a6b12c83d12bc5e47146d9b2fd2ce9279
Alaneme Anita <anitaalaneme@gmail.com>
2025-10-06 15:51:01 +0100

    feat: add CommentViewSet and register /api/comments/ endpoint

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from .models import Post, Comment
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, CommentSerializer
 from django.http import HttpResponse
 
 def home(request):
@@ -11,8 +12,18 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
 def create(self, request, *args, **kwargs):
         try:
             return super().create(request, *args, **kwargs)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
