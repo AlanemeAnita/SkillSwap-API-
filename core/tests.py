fbc9a4e63ca88a36ce14ae870744dcd6f78122b0
Alaneme Anita <anitaalaneme@gmail.com>
2025-10-06 15:52:24 +0100

    test: add tests for Comment creation

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
+from .models import Post, UserProfile, Comment
 from core.models import UserProfile
 
 class PostTests(TestCase):
@@ -21,3 +22,15 @@ class PostTests(TestCase):
     def test_create_post_invalid(self):
         response = self.client.post("/api/posts/", {"title": "Hi", "content": ""})
         self.assertEqual(response.status_code, 400)
+
+class CommentTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='pass123')
+        self.profile = UserProfile.objects.create(user=self.user)
+        self.post = Post.objects.create(title='Test Post', content='Some content', user=self.profile)
+
+    def test_create_comment(self):
+        data = {'post': self.post.id, 'user': self.profile.id, 'content': 'Nice post!'}
+        response = self.client.post('/api/comments/', data)
+        self.assertEqual(response.status_code, 201)
